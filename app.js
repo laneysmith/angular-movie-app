f17d226377f1043bf5941d1dcb56628a79b18e35
Laney Smith <lsmith.unc@gmail.com>
2016-07-24 13:38:50 -0600

    Incorporate omdb api

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
-var routerApp = angular.module('routerApp', ['ui.router']);
-routerApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+var app = angular.module('routerApp', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$urlRouterProvider.otherwise('/home');
 	$stateProvider
 		.state('home', {
 			url: '/home',
-			views: {
-				'': {
-					templateUrl: 'search.html',
-					controller: 'movieList'
-				},
-				'results@home': {
-					templateUrl: 'results.html',
-					controller: 'movieList'
-				}
-			}
+			templateUrl: 'search.html',
+			controller: 'allResultsController'
+		})
+		.state('home.results', {
+			url: '/results',
+			templateUrl: 'results.html',
+			controller: 'allResultsController'
+		})
+		.state('detail', {
+			url: '/results/:movieId',
+			templateUrl: 'detail.html',
+			controller: 'movieDetailsController'
 		})
 }]);
-routerApp.controller('movieList', function($scope, $http) {
-	$scope.showResults = false;
+
+app.controller('movieDetailsController', function($scope, $http, $stateParams) {
+	console.log($stateParams.movieId);
+	$http.get('http://www.omdbapi.com/?i=' + $stateParams.movieId)
+			.then(function(data) {
+				console.log(data.data);
+				$scope.movieDetails = data.data;
+	});
+});
+
+app.controller('allResultsController', function($scope, $http, $state) {
 	$scope.list;
 	$scope.searchMovies = function() {
-		$http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm).then(function(data){
-			console.log(data.data.Search);
-			$scope.list = data.data.Search;
-		});
+		return $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm)
+			.then(function(data) {
+				console.log(data.data.Search);
+				$scope.list = data.data.Search;
+			}).then(function() {
+				console.log('here');
+				console.log('valid?', searchForm.$valid);
+				// if (searchForm.$valid) {
+					// console.log('in it');
+					$state.go(home.results);
+				// }
+			});
 	};
-
 });
